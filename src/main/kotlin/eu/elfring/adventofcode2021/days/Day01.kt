1c9b03becc935ad729dd06ac197f0fa91d4b4283
Tom Elfring <tom.elfring@topicus.nl>
2021-12-01 09:52:23 +0100

    Day 1: Sonar Sweep

--- src/main/kotlin/eu/elfring/adventofcode2021/days/Day01.kt
+++ src/main/kotlin/eu/elfring/adventofcode2021/days/Day01.kt
@@ -6,11 +6,11 @@ class Day01 : AoCPuzzle(1)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        return input.map { it.toInt() }.zipWithNext().count { it.first < it.second }
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        return input.map { it.toInt() }.windowed(3).map { it.sum() }.zipWithNext().count { it.first < it.second }
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2021/Day01Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day01Test.kt
@@ -10,20 +10,29 @@ import org.junit.jupiter.api.Test
 internal class Day01Test
 {
     private val input = """
-        TODO INPUT
+        199
+        200
+        208
+        210
+        200
+        207
+        240
+        269
+        260
+        263
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day01().part1(input))
+        Assertions.assertEquals(7, Day01().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day01().part2(input))
+        Assertions.assertEquals(5, Day01().part2(input))
     }
 }
