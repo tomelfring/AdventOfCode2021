19965337d953be8dd88c6ecd126de3e7678e49a8
Tom <1036527+tomelfring@users.noreply.github.com>
2021-12-02 09:21:30 +0100

    Day 2: Dive! (#2)

--- src/main/kotlin/eu/elfring/adventofcode2021/days/Day02.kt
+++ src/main/kotlin/eu/elfring/adventofcode2021/days/Day02.kt
@@ -6,11 +6,32 @@ class Day02 : AoCPuzzle(2)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val values = input.map(::toDirs).reduce { a, b -> a.first + b.first to a.second + b.second }
+        return values.first * values.second
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val values = input.map(::toDirs).fold(Part2(0, 0, 0)) { acc, value ->
+            Part2(acc.horizontalPosition + value.first,
+                  acc.depth + value.first * acc.aim,
+                  acc.aim + value.second)
+        }
+
+        return values.horizontalPosition * values.depth
     }
+
+    private fun toDirs(input: String): Pair<Int, Int>
+    {
+        val steps = input.substringAfter(" ").toInt()
+        return when (input.substringBefore(" "))
+        {
+            "forward" -> steps to 0
+            "down"    -> 0 to steps
+            "up"      -> 0 to -steps
+            else      -> throw IllegalArgumentException("Unknown statement")
+        }
+    }
+
+    data class Part2(val horizontalPosition: Int, val depth: Int, val aim: Int)
 }
--- src/test/kotlin/eu/elfring/adventofcode2021/Day02Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day02Test.kt
@@ -10,20 +10,25 @@ import org.junit.jupiter.api.Test
 internal class Day02Test
 {
     private val input = """
-        TODO INPUT
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day02().part1(input))
+        Assertions.assertEquals(150, Day02().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day02().part2(input))
+        Assertions.assertEquals(900, Day02().part2(input))
     }
 }
