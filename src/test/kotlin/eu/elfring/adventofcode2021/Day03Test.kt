398a10ea63b0f711999c6da44bc30e495e45a5ca
Tom <1036527+tomelfring@users.noreply.github.com>
2021-12-03 10:01:57 +0100

    Day 3: Binary Diagnostic (#3)

--- src/test/kotlin/eu/elfring/adventofcode2021/Day03Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day03Test.kt
@@ -10,20 +10,31 @@ import org.junit.jupiter.api.Test
 internal class Day03Test
 {
     private val input = """
-        TODO INPUT
+        00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day03().part1(input))
+        Assertions.assertEquals(198, Day03().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day03().part2(input))
+        Assertions.assertEquals(230, Day03().part2(input))
     }
 }
