70ed4d7e3f551cb46d23d6bdb204ed80035aa1b6
Tom Elfring <tom.elfring@topicus.nl>
2021-12-03 10:00:23 +0100

    Day 3: Binary Diagnostic

--- src/main/kotlin/eu/elfring/adventofcode2021/days/Day03.kt
+++ src/main/kotlin/eu/elfring/adventofcode2021/days/Day03.kt
@@ -6,11 +6,43 @@ class Day03 : AoCPuzzle(3)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val mappedInput = input.map { line -> line.toCharArray().map { char -> char.digitToInt() } }
+
+        val firstValue = mappedInput.mostCommonBits()
+        val secondValue = firstValue.map { it xor 1 }
+
+        return firstValue.joinToString("").toInt(2) * secondValue.joinToString("").toInt(2)
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val mappedInput = input.map { line -> line.toCharArray().map { char -> char.digitToInt() } }
+
+        val oxygenRating = solvePart2(mappedInput) { currentBit, mostCommonBit -> currentBit != mostCommonBit }
+        val scrubberRating = solvePart2(mappedInput) { currentBit, mostCommonBit -> currentBit == mostCommonBit }
+
+        return oxygenRating * scrubberRating
+    }
+
+    /**
+     * Get a list of most common entries (int) per bit
+     */
+    private fun List<List<Int>>.mostCommonBits() = first().indices
+        .map { idx -> count { it[idx] == 1 } }
+        .map { if (it >= size - it) 1 else 0 }
+
+
+    private fun solvePart2(input: List<List<Int>>, test: (Int, Int) -> Boolean): Int
+    {
+        val ratings = input.toMutableList()
+        var bitIndex = 0
+        while (ratings.size > 1)
+        {
+            val mostCommonAtCurrentBit = ratings.mostCommonBits()[bitIndex]
+
+            ratings.removeIf { rating -> test(rating[bitIndex], mostCommonAtCurrentBit) }
+            bitIndex++
+        }
+        return ratings.first().joinToString("").toInt(2)
     }
 }
