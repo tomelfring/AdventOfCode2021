bc5484b77f235998c5a20d8fedd37e2ac39a677c
Tom <1036527+tomelfring@users.noreply.github.com>
2021-12-04 08:15:36 +0100

    Day 4: Giant Squid (#4)

--- src/main/kotlin/eu/elfring/adventofcode2021/days/Day04.kt
+++ src/main/kotlin/eu/elfring/adventofcode2021/days/Day04.kt
@@ -6,11 +6,71 @@ class Day04 : AoCPuzzle(4)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val game = parseInput(input)
+
+        var lastNumber = 0
+        while(game.boards.all{ !it.isWinning() })
+        {
+            lastNumber = game.playRound()
+        }
+
+        return game.boards.first { it.isWinning() }.score() * lastNumber
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val game = parseInput(input)
+
+        var lastNumber = 0
+        val winningBoards = mutableListOf<Board>()
+        while(game.boards.any { !it.isWinning() })
+        {
+            lastNumber = game.playRound()
+            game.boards.forEach { if (it.isWinning() && !winningBoards.contains(it)) winningBoards.add(it) }
+        }
+
+        return winningBoards.last().score() * lastNumber
+    }
+
+    private fun parseInput(input: List<String>): BingoGame
+    {
+        val drawnNumbers = input.first().split(",").map { it.toInt() }.toMutableList()
+
+        val boards = input.drop(2).chunked(6).map(::parseBoard)
+
+        return BingoGame(drawnNumbers, boards)
+    }
+
+    private fun parseBoard(boardInput: List<String>): Board
+    {
+        val numbers = boardInput.take(5).map { it.trim().split("\\s+".toRegex()).map { it.toInt() }.toMutableList() }.toMutableList()
+        return Board(numbers)
+    }
+
+    data class BingoGame(val drawnNumbers: MutableList<Int>, val boards: List<Board>)
+    {
+        fun playRound(): Int
+        {
+            val number = drawnNumbers.removeFirst()
+            boards.forEach { it.markNumber(number) }
+
+            return number
+        }
+    }
+
+    data class Board(val numbers: MutableList<MutableList<Int>>)
+    {
+        fun isWinning(): Boolean
+        {
+            val horWin = numbers.any{ it.sum() == -5 }
+
+            val vertWin = (0..4).map { index -> (0..4).map { line -> numbers[line][index] } }.any { it.sum() == -5 }
+
+            return horWin || vertWin
+        }
+
+        fun markNumber(number: Int) = numbers.forEach { line -> line.forEachIndexed { index, i -> if (i == number) line[index] = -1 } }
+
+        fun score() = numbers.sumOf { it.sumOf { if (it == -1) 0 else it } }
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2021/Day04Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day04Test.kt
@@ -10,20 +10,38 @@ import org.junit.jupiter.api.Test
 internal class Day04Test
 {
     private val input = """
-        TODO INPUT
+        7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+        22 13 17 11  0
+         8  2 23  4 24
+        21  9 14 16  7
+         6 10  3 18  5
+         1 12 20 15 19
+        
+         3 15  0  2 22
+         9 18 13 17  5
+        19  8  7 25 23
+        20 11 10 24  4
+        14 21 16 12  6
+        
+        14 21 17 24  4
+        10 16 15  9 19
+        18  8 23 26 20
+        22 11 13  6  5
+         2  0 12  3  7
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day04().part1(input))
+        Assertions.assertEquals(4512, Day04().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day04().part2(input))
+        Assertions.assertEquals(1924, Day04().part2(input))
     }
 }
