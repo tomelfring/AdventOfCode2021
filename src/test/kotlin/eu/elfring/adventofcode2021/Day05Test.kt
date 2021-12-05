bc1109f14deebebedda081f5c6a557123ca637ff
Tom Elfring <tom.elfring@topicus.nl>
2021-12-05 17:17:37 +0100

    Day 5: Hydrothermal Venture

--- src/test/kotlin/eu/elfring/adventofcode2021/Day05Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day05Test.kt
@@ -10,20 +10,29 @@ import org.junit.jupiter.api.Test
 internal class Day05Test
 {
     private val input = """
-        TODO INPUT
+        0,9 -> 5,9
+        8,0 -> 0,8
+        9,4 -> 3,4
+        2,2 -> 2,1
+        7,0 -> 7,4
+        6,4 -> 2,0
+        0,9 -> 2,9
+        3,4 -> 1,4
+        0,0 -> 8,8
+        5,5 -> 8,2
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day05().part1(input))
+        Assertions.assertEquals(5, Day05().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day05().part2(input))
+        Assertions.assertEquals(12, Day05().part2(input))
     }
 }
