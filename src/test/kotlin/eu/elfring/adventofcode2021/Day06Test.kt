e95617b0fc05b54bc55ffd097499c246ebd704fb
Tom Elfring <tom.elfring@topicus.nl>
2021-12-06 08:42:14 +0100

    Testcases omgezet naar Long

--- src/test/kotlin/eu/elfring/adventofcode2021/Day06Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day06Test.kt
@@ -17,13 +17,13 @@ internal class Day06Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(5934, Day06().part1(input))
+        Assertions.assertEquals(5934L, Day06().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(26984457539, Day06().part2(input))
+        Assertions.assertEquals(26984457539L, Day06().part2(input))
     }
 }
