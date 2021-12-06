1245188bde308668da45b406a77a3d6622560307
Tom <1036527+tomelfring@users.noreply.github.com>
2021-12-06 08:43:34 +0100

    Day 6: Lanternfish (#6)
    * Day 6: Lanternfish
    
    * Testcases omgezet naar Long

--- src/main/kotlin/eu/elfring/adventofcode2021/days/Day06.kt
+++ src/main/kotlin/eu/elfring/adventofcode2021/days/Day06.kt
@@ -6,11 +6,42 @@ class Day06 : AoCPuzzle(6)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        var fishMap = parseInput(input.first())
+
+        (1..80).forEach { _ ->
+            fishMap = tick(fishMap, 6, 8)
+        }
+
+        return fishMap.values.sum()
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        var fishMap = parseInput(input.first())
+
+        (1..256).forEach { _ ->
+            fishMap = tick(fishMap, 6, 8)
+        }
+
+        return fishMap.values.sumOf { it }
+    }
+
+    private fun tick(fishMap: Map<Int, Long>, oldFish: Int, newFish: Int): Map<Int, Long>
+    {
+        val newFishMap = mutableMapOf<Int, Long>()
+        (0 until newFish).forEach {
+            newFishMap[it] = fishMap.getOrDefault(it + 1, 0)
+        }
+
+        val newFishCount = fishMap.getOrDefault(0, 0)
+        newFishMap[oldFish] = newFishMap.getOrDefault(oldFish, 0) + newFishCount
+        newFishMap[newFish] = newFishCount
+
+        return newFishMap
+    }
+
+    private fun parseInput(input: String): Map<Int, Long>
+    {
+        return input.split(",").map { it.toInt() }.groupBy { it }.mapValues { it.value.count().toLong() }
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2021/Day06Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day06Test.kt
@@ -10,20 +10,20 @@ import org.junit.jupiter.api.Test
 internal class Day06Test
 {
     private val input = """
-        TODO INPUT
+        3,4,3,1,2
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day06().part1(input))
+        Assertions.assertEquals(5934L, Day06().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day06().part2(input))
+        Assertions.assertEquals(26984457539L, Day06().part2(input))
     }
 }
