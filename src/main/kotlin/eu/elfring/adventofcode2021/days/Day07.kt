acecc18ccb568f48ccf25b4f685cd5f52e3366b5
Tom <1036527+tomelfring@users.noreply.github.com>
2021-12-07 09:10:16 +0100

    Day 7: The Treachery of Whales (#7)

--- src/main/kotlin/eu/elfring/adventofcode2021/days/Day07.kt
+++ src/main/kotlin/eu/elfring/adventofcode2021/days/Day07.kt
@@ -1,16 +1,26 @@
 package eu.elfring.adventofcode2021.days
 
 import eu.elfring.adventofcode2021.AoCPuzzle
+import kotlin.math.absoluteValue
 
 class Day07 : AoCPuzzle(7)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        return calculateLeastFuelUsage(input.first()) { it }
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        return calculateLeastFuelUsage(input.first()) { ((it / 2.0) * (it + 1)).toInt() }
+    }
+
+    private fun calculateLeastFuelUsage(input: String, fuelCalculation: (Int) -> Int): Int
+    {
+        val mapped = input.split(",").map { it.toInt() }
+        val min = mapped.minOf { it }
+        val max = mapped.maxOf { it }
+
+        return (min..max).map { horPos -> mapped.sumOf { crabPos -> fuelCalculation((crabPos - horPos).absoluteValue) } }.minOf { it }
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2021/Day07Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day07Test.kt
@@ -10,20 +10,20 @@ import org.junit.jupiter.api.Test
 internal class Day07Test
 {
     private val input = """
-        TODO INPUT
+        16,1,2,0,4,2,7,1,2,14
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day07().part1(input))
+        Assertions.assertEquals(37, Day07().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day07().part2(input))
+        Assertions.assertEquals(168, Day07().part2(input))
     }
 }
