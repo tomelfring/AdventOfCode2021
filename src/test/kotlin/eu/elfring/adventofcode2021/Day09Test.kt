7fe33ab7cad7689e97e601998cd77f511e173816
Tom Elfring <tom.elfring@topicus.nl>
2021-12-09 09:54:36 +0100

    Day 9: Smoke Basin

--- src/test/kotlin/eu/elfring/adventofcode2021/Day09Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day09Test.kt
@@ -10,20 +10,24 @@ import org.junit.jupiter.api.Test
 internal class Day09Test
 {
     private val input = """
-        TODO INPUT
+        2199943210
+        3987894921
+        9856789892
+        8767896789
+        9899965678
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day09().part1(input))
+        Assertions.assertEquals(15, Day09().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day09().part2(input))
+        Assertions.assertEquals(1134, Day09().part2(input))
     }
 }
