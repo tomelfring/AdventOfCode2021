981ba1c2c9c83869f46c76108910d92c8f50d5a1
Tom <1036527+tomelfring@users.noreply.github.com>
2021-12-10 08:52:11 +0100

    Day 10: Syntax Scoring (#10)

--- src/main/kotlin/eu/elfring/adventofcode2021/days/Day10.kt
+++ src/main/kotlin/eu/elfring/adventofcode2021/days/Day10.kt
@@ -4,13 +4,50 @@ import eu.elfring.adventofcode2021.AoCPuzzle
 
 class Day10 : AoCPuzzle(10)
 {
+    private val corruptedScore = mapOf<String, Long>(
+        ")" to 3,
+        "]" to 57,
+        "}" to 1197,
+        ">" to 25137
+    )
+
+    private val incompleteScore = mapOf(
+        '(' to 1,
+        '[' to 2,
+        '{' to 3,
+        '<' to 4
+    )
+
     override fun part1(input: List<String>): Any
     {
-        return 0
+        return input.map(::checkSyntax).sumOf { if (it.first) it.second else 0 }
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val scores = input.map(::checkSyntax).mapNotNull { if (!it.first) it.second else null }.sorted()
+
+        return scores[scores.size/2]
+    }
+
+    private fun checkSyntax(input: String): Pair<Boolean, Long>
+    {
+        val openPairs = mutableListOf<Char>()
+
+        input.toCharArray().forEach {
+            when(it)
+            {
+                '(' -> openPairs.add(it)
+                '[' -> openPairs.add(it)
+                '{' -> openPairs.add(it)
+                '<' -> openPairs.add(it)
+                ')' -> if (openPairs.last() != '(') return true to corruptedScore.getOrElse(")") { throw Exception() } else openPairs.removeLast()
+                ']' -> if (openPairs.last() != '[') return true to corruptedScore.getOrElse("]") { throw Exception() } else openPairs.removeLast()
+                '}' -> if (openPairs.last() != '{') return true to corruptedScore.getOrElse("}") { throw Exception() } else openPairs.removeLast()
+                '>' -> if (openPairs.last() != '<') return true to corruptedScore.getOrElse(">") { throw Exception() } else openPairs.removeLast()
+            }
+        }
+
+        return false to openPairs.reversed().fold(0L) { acc, value -> acc * 5 + incompleteScore.getOrElse(value) { throw Exception() } }
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2021/Day10Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day10Test.kt
@@ -10,20 +10,29 @@ import org.junit.jupiter.api.Test
 internal class Day10Test
 {
     private val input = """
-        TODO INPUT
+        [({(<(())[]>[[{[]{<()<>>
+        [(()[<>])]({[<{<<[]>>(
+        {([(<{}[<>[]}>{[]{[(<()>
+        (((({<>}<{<{<>}{[]{[]{}
+        [[<[([]))<([[{}[[()]]]
+        [{[{({}]{}}([{[{{{}}([]
+        {<[[]]>}<{[{[{[]{()[[[]
+        [<(<(<(<{}))><([]([]()
+        <{([([[(<>()){}]>(<<{{
+        <{([{{}}[<[[[<>{}]]]>[]]
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day10().part1(input))
+        Assertions.assertEquals(26397L, Day10().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day10().part2(input))
+        Assertions.assertEquals(288957L, Day10().part2(input))
     }
 }
