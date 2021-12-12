63444b107b6e2327e2e4764609db310557d8a83e
Tom Elfring <tom.elfring@topicus.nl>
2021-12-12 18:06:31 +0100

    Day 12: Passage Pathing

--- src/main/kotlin/eu/elfring/adventofcode2021/days/Day12.kt
+++ src/main/kotlin/eu/elfring/adventofcode2021/days/Day12.kt
@@ -6,11 +6,40 @@ class Day12 : AoCPuzzle(12)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        return Passage(input).getPathCount(false)
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        return Passage(input).getPathCount(true)
+    }
+
+    class Passage(private val input: List<String>)
+    {
+        private val edges = mutableMapOf<String, Set<String>>().withDefault { setOf() }.apply {
+            input.map { it.split("-") }.forEach { (a, b) ->
+                put(a, getValue(a) + b)
+                put(b, getValue(b) + a)
+            }
+        }
+
+        fun getPathCount(allowExtraCave: Boolean): Int
+        {
+            return searchRecursiveDfs("start", allowExtraCave).size
+        }
+
+        private fun searchRecursiveDfs(curr: String, allowExtraCave: Boolean, path: List<String> = listOf()): List<List<String>>
+        {
+            val updatedPath = path + curr
+            if (curr == "end") return listOf(updatedPath)
+
+            return edges.getValue(curr).filterNot { next ->
+                next == "start" ||
+                next.isLower() && next in updatedPath &&
+                if (allowExtraCave) updatedPath.filter { it.isLower() }.groupingBy { it }.eachCount().values.any { it >= 2 } else true
+            }.flatMap { searchRecursiveDfs(it, allowExtraCave, updatedPath) }
+        }
+
+        private fun String.isLower() = this.all { it.isLowerCase() }
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2021/Day12Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day12Test.kt
@@ -10,20 +10,37 @@ import org.junit.jupiter.api.Test
 internal class Day12Test
 {
     private val input = """
-        TODO INPUT
+        fs-end
+        he-DX
+        fs-he
+        start-DX
+        pj-DX
+        end-zg
+        zg-sl
+        zg-pj
+        pj-he
+        RW-he
+        fs-DX
+        pj-RW
+        zg-RW
+        start-pj
+        he-WI
+        zg-he
+        pj-fs
+        start-RW
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day12().part1(input))
+        Assertions.assertEquals(226, Day12().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day12().part2(input))
+        Assertions.assertEquals(3509, Day12().part2(input))
     }
 }
