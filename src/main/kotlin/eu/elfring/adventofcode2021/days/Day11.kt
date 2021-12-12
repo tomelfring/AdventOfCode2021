416fffed3d00fc5910ae3775264520538044afa0
Tom Elfring <tom.elfring@topicus.nl>
2021-12-12 18:04:32 +0100

    Day 11: Dumbo Octopus

--- src/main/kotlin/eu/elfring/adventofcode2021/days/Day11.kt
+++ src/main/kotlin/eu/elfring/adventofcode2021/days/Day11.kt
@@ -6,11 +6,115 @@ class Day11 : AoCPuzzle(11)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val splitInput = input.map { it.toCharArray().map{ char -> char.digitToInt() to false }.toMutableList() }.toMutableList()
+
+        var sum = 0
+        repeat(100) {
+            sum += doTick(splitInput)
+        }
+
+        return sum
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val splitInput = input.map { it.toCharArray().map{ char -> char.digitToInt() to false }.toMutableList() }.toMutableList()
+
+        val octoCount = splitInput.size * splitInput.first().size
+
+        var count = 1
+        while (doTick(splitInput) != octoCount)
+        {
+            count++
+        }
+        return count
+    }
+
+    private fun doTick(input: MutableList<MutableList<Pair<Int, Boolean>>>): Int
+    {
+        // Add one to every value
+        input.forEachIndexed { y, pairs ->
+            pairs.forEachIndexed{ x, value ->
+                input[y][x] = value.first+1 to value.second
+            }
+        }
+
+        // Do flashes
+        doFlashes(input)
+
+        // Count flashes & reset to 0 if > 9
+        var flashes = 0
+        input.forEachIndexed { y, pairs ->
+            pairs.forEachIndexed { x, pair ->
+                if (pair.first > 9 && pair.second)
+                {
+                    flashes++
+                    input[y][x] = 0 to false
+                }
+            }
+        }
+
+        return flashes
+    }
+
+    private fun doFlashes(input: MutableList<MutableList<Pair<Int, Boolean>>>)
+    {
+        var hasAnyFlashed = true
+        while (hasAnyFlashed)
+        {
+            hasAnyFlashed = false
+            input.forEachIndexed { y, pairs ->
+                pairs.forEachIndexed { x, pair ->
+                    if (pair.first > 9 && !pair.second)
+                    {
+                        hasAnyFlashed = true
+                        input[y][x] = pair.first to true
+
+                        if (y > 0)
+                        {
+                            // y-1 x-1
+                            if (x > 0)
+                            {
+                                input[y-1][x-1] = input[y-1][x-1].first+1 to input[y-1][x-1].second
+                            }
+                            // y-1 x
+                            input[y-1][x] = input[y-1][x].first+1 to input[y-1][x].second
+                            // y-1 x+1
+                            if (x < pairs.size-1)
+                            {
+                                input[y-1][x+1] = input[y-1][x+1].first+1 to input[y-1][x+1].second
+                            }
+                        }
+
+                        // y x-1
+                        if (x > 0)
+                        {
+                            input[y][x-1] = input[y][x-1].first+1 to input[y][x-1].second
+                        }
+                        // y x+1
+                        if (x < pairs.size-1)
+                        {
+                            input[y][x+1] = input[y][x+1].first+1 to input[y][x+1].second
+                        }
+
+                        if (y < input.size-1)
+                        {
+                            // y+1 x-1
+                            if (x > 0)
+                            {
+                                input[y+1][x-1] = input[y+1][x-1].first+1 to input[y+1][x-1].second
+                            }
+                            // y+1 x
+                            input[y+1][x] = input[y+1][x].first+1 to input[y+1][x].second
+                            // y+1 x+1
+                            if (x < pairs.size-1)
+                            {
+                                input[y+1][x+1] = input[y+1][x+1].first+1 to input[y+1][x+1].second
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2021/Day11Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day11Test.kt
@@ -10,20 +10,29 @@ import org.junit.jupiter.api.Test
 internal class Day11Test
 {
     private val input = """
-        TODO INPUT
+        5483143223
+        2745854711
+        5264556173
+        6141336146
+        6357385478
+        4167524645
+        2176841721
+        6882881134
+        4846848554
+        5283751526
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day11().part1(input))
+        Assertions.assertEquals(1656, Day11().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day11().part2(input))
+        Assertions.assertEquals(195, Day11().part2(input))
     }
 }
