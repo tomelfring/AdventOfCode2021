a238ef046cdbdb9d6564c80ced003286559131a7
Tom Elfring <tom.elfring@topicus.nl>
2021-12-13 08:57:06 +0100

    Day 13: Transparent Origami

--- src/test/kotlin/eu/elfring/adventofcode2021/Day13Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day13Test.kt
@@ -10,20 +10,40 @@ import org.junit.jupiter.api.Test
 internal class Day13Test
 {
     private val input = """
-        TODO INPUT
+        6,10
+        0,14
+        9,10
+        0,3
+        10,4
+        4,11
+        6,0
+        6,12
+        4,1
+        0,13
+        10,12
+        3,4
+        3,0
+        8,4
+        1,10
+        2,14
+        8,10
+        9,0
+        
+        fold along y=7
+        fold along x=5
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day13().part1(input))
+        Assertions.assertEquals(17, Day13().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day13().part2(input))
+        Assertions.assertEquals(listOf("", "#####", "#   #", "#   #", "#   #", "#####").joinToString("\n"), Day13().part2(input))
     }
 }
