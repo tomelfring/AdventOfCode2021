a7fb9036f63d2b95521bd5317f50a3788e169cd0
Tom <1036527+tomelfring@users.noreply.github.com>
2021-12-14 09:52:21 +0100

    Day 14: Extended Polymerization (#14)

--- src/main/kotlin/eu/elfring/adventofcode2021/days/Day14.kt
+++ src/main/kotlin/eu/elfring/adventofcode2021/days/Day14.kt
@@ -6,11 +6,59 @@ class Day14 : AoCPuzzle(14)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val (template, pairInsertionRules) = parseInput(input)
+
+        return doRun(template, 10, pairInsertionRules)
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val (template, pairInsertionRules) = parseInput(input)
+
+        return doRun(template, 40, pairInsertionRules)
+    }
+
+    private fun parseInput(input: List<String>): Pair<String, Map<String, List<String>>>
+    {
+        val template = input.first()
+        val regex = """([A-Z]+) -> ([A-Z]+)""".toRegex()
+        val pairInsertionRules = input.drop(2)
+            .associate { regex.matchEntire(it)!!.destructured.let { (a, b) -> a to listOf(a[0] + b, b + a[1]) } }
+        return template to pairInsertionRules
+    }
+
+    private fun doRun(template: String, steps: Int, pairInsertionRules: Map<String, List<String>>): Long
+    {
+        var state = template.windowed(2).groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+
+        repeat(steps) {
+            // Map each pair to the next set of pairs
+            state = mutableMapOf<String, Long>().withDefault { 0 }.apply {
+                for ((src, n) in state)
+                {
+                    for (dst in pairInsertionRules.getValue(src))
+                    {
+                        put(dst, getValue(dst) + n)
+                    }
+                }
+            }
+        }
+
+        val characterCounts = mutableMapOf<Char, Long>().withDefault { 0 }.apply {
+            // Each value, except first & last is counted double, add them
+            put(template.first(), 1)
+            put(template.last(), getValue(template.last()) + 1)
+
+            for ((pair, n) in state)
+            {
+                for (c in pair)
+                {
+                    put(c, getValue(c) + n)
+                }
+            }
+        }
+
+        // As each is double counted, divide by 2
+        return (characterCounts.values.maxOf { it } - characterCounts.values.minOf { it }) / 2
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2021/Day14Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day14Test.kt
@@ -10,20 +10,37 @@ import org.junit.jupiter.api.Test
 internal class Day14Test
 {
     private val input = """
-        TODO INPUT
+        NNCB
+        
+        CH -> B
+        HH -> N
+        CB -> H
+        NH -> C
+        HB -> C
+        HC -> B
+        HN -> C
+        NN -> C
+        BH -> H
+        NC -> B
+        NB -> B
+        BN -> B
+        BB -> N
+        BC -> B
+        CC -> N
+        CN -> C
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day14().part1(input))
+        Assertions.assertEquals(1588L, Day14().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day14().part2(input))
+        Assertions.assertEquals(2188189693529, Day14().part2(input))
     }
 }
