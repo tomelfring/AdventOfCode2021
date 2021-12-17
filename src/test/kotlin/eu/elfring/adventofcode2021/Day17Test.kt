59d0bfc7415045230b44d2a9a2652be875ab7439
Tom <1036527+tomelfring@users.noreply.github.com>
2021-12-17 08:55:03 +0100

    Day 17: Trick Shot (#17)

--- src/test/kotlin/eu/elfring/adventofcode2021/Day17Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2021/Day17Test.kt
@@ -10,20 +10,20 @@ import org.junit.jupiter.api.Test
 internal class Day17Test
 {
     private val input = """
-        TODO INPUT
+        target area: x=20..30, y=-10..-5
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day17().part1(input))
+        Assertions.assertEquals(45, Day17().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day17().part2(input))
+        Assertions.assertEquals(112, Day17().part2(input))
     }
 }
